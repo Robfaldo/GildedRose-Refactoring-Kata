7e4377773d1b2f910a99812d8bd3d80a7b3308f1
Robert Faldo <robertfaldo@gmail.com>
2018-07-17 13:44:52 +0100

    Add feature test

--- js/spec/features/gilded_rose_spec.js
+++ js/spec/features/gilded_rose_spec.js
@@ -1,11 +1,16 @@
 describe("Gilded Rose", function() {
+  describe('.updateQuality', () => {
+    it('updates the quality and sellin property values', () => {
+      const itemInput = [ new Item("foo", 5, 10) ]
+      const gildedRose = new Shop(itemInput);
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+      const updatedItems = gildedRose.updateQuality()
+      const firstItem = updatedItems[0]
+
+      expect(firstItem.quality).toEqual(9)
+      expect(firstItem.sellIn).toEqual(4)
+    });
   });
-
 });
 
 // Once the sell by date has passed, Quality degrades twice as fast
