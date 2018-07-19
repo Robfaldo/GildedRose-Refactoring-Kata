39c9d4068036cfefdf0eb1bcc51abe209c3d4a41
Robert Faldo <robertfaldo@gmail.com>
2018-07-19 18:09:12 +0100

    Add Unit tests for AgedBrie

--- js/spec/aged_brie_spec.js
+++ js/spec/aged_brie_spec.js
@@ -11,4 +11,46 @@ describe('AgedBrie', () => {
       expect(agedBrie).toEqual(inputItem)
     });
   });
+  describe('.update', () => {
+    describe('When name is Aged Brie', () => {
+      describe('When quality is 50', () => {
+        it('only reduces sellIn', () => {
+          const item = {
+            name: "Aged Brie",
+            sellIn: 10,
+            quality: 50,
+          }
+          const expectedResult = {
+            name: 'Aged Brie',
+            sellIn: 9,
+            quality: 50
+          }
+          const agedBrieItem = new AgedBrie(item);
+
+          const result = agedBrieItem.update();
+
+          expect(result).toEqual(expectedResult);
+        });
+      });
+      describe('When quality is under 50', () => {
+        it('increases quality and reduces sellIn', () => {
+          const item = {
+            name: "Aged Brie",
+            sellIn: 3,
+            quality: 30,
+          }
+          const expectedResult = {
+            name: 'Aged Brie',
+            sellIn: 2,
+            quality: 31
+          }
+          const agedBrieItem = new AgedBrie(item);
+
+          const result = agedBrieItem.update();
+
+          expect(result).toEqual(expectedResult);
+        });
+      });
+    });
+  });
 });
--- js/src/aged_brie.js
+++ js/src/aged_brie.js
@@ -4,5 +4,10 @@ class AgedBrie {
     this.sellIn = item.sellIn;
     this.quality = item.quality;
   }
-  
+
+  update() {
+    this.sellIn -= 1;
+    if (this.quality < 50) this.quality += 1;
+    return this
+  }
 }
