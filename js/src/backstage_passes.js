bad8ee6a033977bc46cb61c086f1dddd79c849cf
Robert Faldo <robertfaldo@gmail.com>
2018-07-19 17:27:52 +0100

    Add all Unit tests for BackStagePasses
    I took all the tests from shop for backstage passes and then
    adapted them for appropriate unit tests for BackStagePasses class

--- js/src/backstage_passes.js
+++ js/src/backstage_passes.js
@@ -4,4 +4,18 @@ class BackstagePasses {
     this.sellIn = item.sellIn;
     this.quality = item.quality;
   }
+
+  update() {
+    this.quality += 1;
+    if (this.sellIn >= 1 && this.sellIn <= 5) {
+      this.quality += 2;
+    }
+    if (this.sellIn >= 6 && this.sellIn <= 10) {
+      this.quality += 1;
+    }
+    if (this.quality > 50) this.quality = 50;
+    if (this.sellIn <= 0) this.quality = 0;
+    this.sellIn -= 1;
+    return this;
+  }
 }
