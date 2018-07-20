b7cad692dbffd95c142932f157bc17989a10efa8
Robert Faldo <robertfaldo@gmail.com>
2018-07-20 07:24:26 +0100

    Remove unecersary tests in item_spec

--- js/spec/item_spec.js
+++ js/spec/item_spec.js
@@ -7,23 +7,3 @@ describe('Item', () => {
       expect(item).toEqual(expectedResult)
     });
   });
-  describe('When there are missing paramters', () => {
-    xit('throws an error if missing name', () => {
-
-    });
-    xit('throws an error if missing sellIn value', () => {
-
-    });
-    xit('throws an error if missing sellIn value', () => {
-
-    });
-  });
-  describe('When paramaters are invalid', () => {
-    xit('throws an error if quality is negative', () => {
-
-    });
-    xit('throws an error if quality is over 50', () => {
-
-    });
-  });
-});
