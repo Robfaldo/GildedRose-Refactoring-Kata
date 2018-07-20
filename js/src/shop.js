3ff2780b3b6668d274d4712a0289caa71688d2f2
Robert Faldo <robertfaldo@gmail.com>
2018-07-20 13:38:47 +0100

    Shop uses ItemNameLookup to match item name to class
    Before this, there was a _getClassName(item) and Shop
    stored an object with 'name':className key/value. After
    this Shop uses the ItemNameLookup class to match the name of
    the item to its class

--- js/src/shop.js
+++ js/src/shop.js
@@ -1,37 +1,21 @@
 class Shop {
-  constructor(items=[], itemNames = true){
+  constructor(items=[], itemNames = new ItemNameLookup()){
     this.items = [];
-    this.itemNamesLookup = {
-      'Conjured': Conjured,
-      'Backstage passes to a TAFKAL80ETC concert': BackstagePasses,
-      'Aged Brie': AgedBrie,
-      'Sulfuras, Hand of Ragnaros': Sulfuras
-    }
+    this.itemNameLookup = itemNames;
 
     for (var i = 0; i < items.length; i++) {
-      var className = this._getClassName(items[i]);
+      var className = this.itemNameLookup.match(items[i].name);
       var newItem = new className(items[i]);
       this.items.push(newItem);
     }
   }
-
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       this.items[i].update();
     }
     return this.items;
   }
-  _getClassName(item) {
-    var lookupReturn = this.itemNamesLookup[item.name]
-    return lookupReturn === undefined ? Normal : lookupReturn
-  }
 }
 
-// Shop.prototype.itemNames = {
-//     'Conjured': Conjured,
-//     'Backstage passes to a TAFKAL80ETC concert': BackstagePasses,
-//     'Aged Brie': AgedBrie,
-//     'Sulfuras, Hand of Ragnaros': Sulfuras
-// }
 
 module.exports = Shop
